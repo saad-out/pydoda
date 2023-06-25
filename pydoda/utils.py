ec9b918bdd6344344efff81ae6912c16c1e26fa5
saad-out <outsaad03@gmail.com>
2023-06-25 17:03:39 +0100

    globalize dataset path and custom categories

---
+++ pydoda/utils.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+DATASET_DIR = '../doda-dataset'
+
+CUSTOM_CATEGORIES = [
+    'malenames',
+    'femalenames',
+    '(in)definite',
+    'conjug_past',
+    'conjug_present',
+    'imperetives',
+    'masculine_feminine_plural',
+    'verb_to_noun',
+    'idioms',
+    'proverbs',
+]
