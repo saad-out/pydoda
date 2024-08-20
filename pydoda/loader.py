a8ad39211b1aed75ed45a62ccf4864b9d7cb1b37
saad-out <outsaad03@gmail.com>
2024-08-20 18:55:34 +0100

    suppress .fillna() warning

--- pydoda/loader.py
+++ pydoda/loader.py
@@ -103,7 +103,9 @@ class Loader:
             A list of dictionaries containing the values of the specified rows.
         """
         try:
-            return self.__df.loc[self.__df[column].str.contains(value).fillna(False)].to_dict(orient='records')
+            filtered_df = self.__df.loc[self.__df[column].str.contains(value, na=False)]
+            filtered_df = filtered_df.infer_objects(copy=False)
+            return filtered_df.to_dict(orient='records')
         except KeyError:
             raise ValueError(f'Invalid column name: {column}')
         except ValueError:
